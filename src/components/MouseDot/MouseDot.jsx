32198756bb18408fdada0d215ed14e13b4cc81a7
mk <kanaradze223@gmail.com>
2019-02-26 16:19:13 +0200

    mouse dot 1

--- src/components/MouseDot/MouseDot.jsx
+++ src/components/MouseDot/MouseDot.jsx
@@ -1,13 +1,30 @@
+// libs
 import React, { Component } from 'react';
+// hoc
+import withMouse from '../../hoc/withMouse';
+// styles
+import './index.scss';
 
 class MouseDot extends Component {
   render() {
+    const {
+      mouse: { x, y }
+    } = this.props;
+    const mouseStyles = {
+      top: y,
+      left: x,
+    };
+
     return (
-      <div>
-        mousedot
+      <div className="mousedot">
+        <div className="mouse" style={mouseStyles}></div>
+        <div>
+          {'mousedot '.repeat(500)}
+        </div>
+        <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-1519849372.jpg" alt=""/>
       </div>
     );
   }
 }
 
-export default MouseDot;
+export default withMouse(MouseDot);
