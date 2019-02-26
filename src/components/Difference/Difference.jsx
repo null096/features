9d31232e48c7971887423cedabc57e43fce0bfc6
mk <kanaradze223@gmail.com>
2019-02-26 12:55:11 +0200

    diff

--- src/components/Difference/Difference.jsx
+++ src/components/Difference/Difference.jsx
@@ -6,11 +6,15 @@ import './index.scss';
 class Difference extends Component {
   render() {
     return (
-      <>
+      <div class="difference-page">
+        <div className="fixed-text-container">
+          This text is fixed
+        </div>
         {[...Array(5)].map((_, i) => (
           <div key={i} className="container"></div>
         ))}
-      </>
+        <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-1519849372.jpg" alt=""/>
+      </div>
     );
   }
 }
