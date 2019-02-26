2f2aa0dcd4f8f761d89e8c2ec9bbf4b1cef48283
mk <kanaradze223@gmail.com>
2019-02-26 15:04:18 +0200

    mouse dot

---
+++ src/components/MouseDot/MouseDot.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MouseDot extends Component {
+  render() {
+    return (
+      <div>
+        mousedot
+      </div>
+    );
+  }
+}
+
+export default MouseDot;
