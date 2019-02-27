b34989cab1c6c6f821b5a4e26c26db8f30af1067
mk <kanaradze223@gmail.com>
2019-02-27 13:27:18 +0200

    added afterUpAfterDown parameter to withMouse hoc

--- src/hoc/withMouse.jsx
+++ src/hoc/withMouse.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useReducer } from 'react';
+import usePrevious from '../hooks/usePrevious';
 
 const t = {
   ON_MOUSE_MOVE: 'ON_MOUSE_MOVE',
   ON_MOUSE_DOWN: 'ON_MOUSE_DOWN',
-  ON_MOUSE_UP: 'ON_MOUSE_UP'
+  ON_MOUSE_UP: 'ON_MOUSE_UP',
+  ON_UP_DOWN_CHANGE: 'ON_UP_AFTER_DOWN'
 };
 
 const initialState = {
   x: null,
   y: null,
   isDown: false,
+  isUpAfterDown: false,
 };
 
 function reducer(state, action) {
@@ -27,35 +30,56 @@ function reducer(state, action) {
       ...state,
       isDown: false
     };
-    default: return;
+    case t.ON_UP_DOWN_CHANGE: return {
+      ...state,
+      isUpAfterDown: action.isUpAfterDown
+    };
+    default: return state;
   }
 }
 
 const withMouse = (Component) => {
   return (props) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    const onMouseMove = ({ x, y } = {}) => dispatch({
-      type: t.ON_MOUSE_MOVE,
-      x,
-      y,
-    });
-    const onMouseDown = () => dispatch({
-      type: t.ON_MOUSE_DOWN
-    });
-    const onMouseUp = () => dispatch({
-      type: t.ON_MOUSE_UP
-    });
+    const prevState = usePrevious(state);
+    const prevIsDown = (prevState || {}).isDown;
 
     useEffect(() => {
+      const onMouseMove = ({ x, y } = {}) => dispatch({
+        type: t.ON_MOUSE_MOVE,
+        x,
+        y,
+      });
+      const onMouseDown = () => dispatch({
+        type: t.ON_MOUSE_DOWN
+      });
+      const onMouseUp = () => dispatch({
+        type: t.ON_MOUSE_UP
+      });
+
       window.addEventListener('mousemove', onMouseMove, false);
       window.addEventListener('mousedown', onMouseDown, false);
       window.addEventListener('mouseup', onMouseUp, false);
+
       return () => {
         window.removeEventListener('mousemove', onMouseMove, false);
         window.removeEventListener('mousedown', onMouseDown, false);
         window.removeEventListener('mouseup', onMouseUp, false);
       }
     }, []);
+
+    useEffect(()=> {
+      const onUpAfterDownChange = (val) => dispatch({
+        type: t.ON_UP_DOWN_CHANGE,
+        isUpAfterDown: val
+      });
+
+      if (prevIsDown && !state.isDown) {
+        onUpAfterDownChange(true);
+      } else if (state.isUpAfterDown) {
+        onUpAfterDownChange(false);
+      }
+    }, [prevIsDown, state.isDown]);
 
     return (
       <Component
