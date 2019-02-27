179acfc3a4f87f78e81a746989aaa3262a4eb84f
mk <kanaradze223@gmail.com>
2019-02-27 18:07:54 +0200

    add react-pose for dot-shadow animation control

--- src/components/MouseDot/MouseDot.jsx
+++ src/components/MouseDot/MouseDot.jsx
@@ -1,6 +1,7 @@
 // libs
 import React, { Component } from 'react';
 import classNames from 'classnames';
+import posed from 'react-pose';
 // hoc
 import withMouse from '../../hoc/withMouse';
 // styles
@@ -8,16 +9,38 @@ import './index.scss';
 // constants
 const outerDotSize = 11;
 
+const MouseClickShadow = posed.div({
+  visible: {
+    opacity: .8,
+    transition: {
+      opacity: { duration: 0 }
+    }
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      opacity: { duration: 0 }
+    }
+  }
+});
+
 class MouseDot extends Component {
+  // state = {
+  //   dotShadows: []
+  // };
+  // static getDerivedStateFromProps(nextProps, prevState) {
+    
+  // }
   render() {
     const {
-      mouse: { x, y, isDown }  
+      mouse: { x, y, isDown, isUpAfterDown }
     } = this.props;
     const dotOuterStyles = {
       width: `${outerDotSize}px`,
       height: `${outerDotSize}px`,
       top: `${y - outerDotSize / 2}px`,
-      left: `${x - outerDotSize / 2}px`
+      left: `${x - outerDotSize / 2}px`,
+      display: x === null && y === null ? 'none' : ''
     };
     const dotOuterClass = classNames(
       'dot-outer',
@@ -28,6 +51,12 @@ class MouseDot extends Component {
       <div className="mousedot">
         <div className={dotOuterClass} style={dotOuterStyles}>
           <div className="dot-inner"></div>
+        </div>
+        <div>
+          <MouseClickShadow
+            className="dot-shadow"
+            pose={isDown ? 'visible' : 'hidden'}
+          />
         </div>
         <div>
           {'mousedot '.repeat(500)}
